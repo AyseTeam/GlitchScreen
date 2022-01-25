9933913629af1b6512b15422cb488f972854f76c
AyseTeam <86293015+AyseTeam@users.noreply.github.com>
2022-01-25 16:49:53 +0100

    Add files via upload

---
+++ Reload.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlitchScreen
+{
+    public partial class Reload : Form
+    {
+        public Reload()
+        {
+            InitializeComponent();
+        }
+
+        private void Reload_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void Reload_Load(object sender, EventArgs e)
+        {
+            var GoBack = new Form1();
+            GoBack.ShowDialog();
+        }
+    }
+}
