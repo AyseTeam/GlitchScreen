ee1dfab09c533b2effc229df34f662d6821f1848
AyseTeam <86293015+AyseTeam@users.noreply.github.com>
2022-01-26 18:20:40 +0100

    Add files via upload

---
+++ GlitchScreenFramework/GlitchScreenFramework/Reload.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace GlitchScreenFramework
+{
+    partial class Reload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Reload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(12, 12);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Reload";
+            this.Opacity = 0.01D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Reload";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Reload_FormClosing);
+            this.Load += new System.EventHandler(this.Reload_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ GlitchScreenFramework/GlitchScreenFramework/Reload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlitchScreenFramework
+{
+    public partial class Reload : Form
+    {
+        public Reload()
+        {
+            InitializeComponent();
+        }
+
+        private void Reload_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void Reload_Load(object sender, EventArgs e)
+        {
+            var GoBack = new Form1();
+            GoBack.ShowDialog();
+        }
+    }
+}
